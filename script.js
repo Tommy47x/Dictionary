19421463bae7470aaba27053b438df4fedd0861d
Tommy47x <110035943+Tommy47x@users.noreply.github.com>
2023-08-20 09:27:27 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const dictionary = new Set();
+function saveWord() {
+  const newWord = document.getElementById("newWord").value.trim();
+  if (newWord) {
+    dictionary.add(newWord);
+    alert("Word saved!");
+    document.getElementById("newWord").value = "";
+  }
+}
+
+function wordCheck() {
+  const searchWordInput = document.getElementById("searchWord");
+  const wordToSearch = searchWordInput.value.trim();
+  const resultMessage = document.getElementById("resultMessage");
+  
+  resultMessage.innerHTML = dictionary.has(wordToSearch)
+    ? `<p class="text-success">${wordToSearch} is in the dictionary.</p>`
+    : `<p class="text-danger">${wordToSearch} is not in the dictionary.</p>`;
+    
+  searchWordInput.value = "";
+}
