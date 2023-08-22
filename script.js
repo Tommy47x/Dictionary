3c9831a05ce669425360ca531f0b4257fd2e6251
Tommy47x <110035943+Tommy47x@users.noreply.github.com>
2023-08-22 11:40:55 +0300

    Update script.js
    -changed the ternary operators with if / else conditions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dictionary = new Set();
 function saveWord() {
+
   const newWord = document.getElementById("newWord").value;
   if (newWord) {
     dictionary.add(newWord);
@@ -8,14 +9,20 @@ function saveWord() {
   }
 }
 
+
 function wordCheck() {
   const searchWordInput = document.getElementById("searchWord");
   const wordToSearch = searchWordInput.value;
   const resultMessage = document.getElementById("resultMessage");
-  
-  resultMessage.innerHTML = dictionary.has(wordToSearch)
-    ? `<p class="text-success">${wordToSearch} is in the dictionary.</p>`
-    : `<p class="text-danger">${wordToSearch} is not in the dictionary.</p>`;
-    
+
+  let message;
+  if (dictionary.has(wordToSearch)) {
+    message = `<p class="text-success">${wordToSearch} is in the dictionary.</p>`;
+  } else {
+    message = `<p class="text-danger">${wordToSearch} is not in the dictionary.</p>`;
+  }
+
+  resultMessage.innerHTML = message;
+
   searchWordInput.value = "";
 }
